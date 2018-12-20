f021f44202d372ce2b00dfdb83e353aa6330e057
Maxim Sloyko <maxim@sloyko.com>
2018-12-19 21:21:11 -0800

    restore: Add functions that print status, when restoring configuration

--- restore.zsh
+++ restore.zsh
@@ -1,19 +1,33 @@
 #!/bin/zsh
 
+if [ ! -n ${ZSH_CUSTOM+1} ]; then
+    echo "Please export ZSH_CUSTOM, then run this script again.";
+    exit 1;
+fi
+
 SCRIPT=$(realpath $0)
 DOT_ROOT=${SCRIPT:h}
 
-if [[ ! -e $HOME/.vimrc ]]; then
-    ln -s $DOT_ROOT/vimrc $HOME/.vimrc
-fi
+link_if_not_exists() {
+    local src=$1;
+    local dst=$2;
 
-if [[ ! -e $HOME/.gvimrc ]]; then
-    ln -s $DOT_ROOT/gvimrc $HOME/.gvimrc
-fi
+    if [[ ! -e $dst ]]; then
+        ln -s $src $dst;
+        echo "$src --> $dst ... link created";
+    else
+        echo "$src --> $dst ... exists, skipping";
+    fi
+}
 
-if [[ ! -e $HOME/.i3 ]]; then
-    ln -s $DOT_ROOT/i3 $HOME/.i3
-fi
+restore_dot() {
+    local fname=$1;
+    link_if_not_exists "${DOT_ROOT}/${fname}" "${HOME}/.${fname}";
+}
+
+restore_dot vimrc
+restore_dot gvimrc
+restore_dot i3
 
 # This requires ZSH_CUSTOM to be exported, which it is not by default
 if [[ ! -e $ZSH_CUSTOM/themes/mxsl.zsh-theme ]]; then
