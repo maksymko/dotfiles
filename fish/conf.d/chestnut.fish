c5a87086f8303622e8734b1019bb14ae75b0b879
Maxim Sloyko <maxims@projecteagle.net>
2026-09-04 16:03:36 -0700

    Chestnut changes

--- fish/conf.d/chestnut.fish
+++ fish/conf.d/chestnut.fish
@@ -1,3 +1,5 @@
 set -gx __pushover_user_key "uL9wbfDhrLr77UiGJ3Eg4VwmgzPbh9"
 set -gx __pushover_api_token "akhxfcrkqxbbfuspt534gpqtju7cx9"
 set -gx __pushover_base_url "https://api.pushover.net/1/messages.json"
+
+set -gx __box_compute 'pje-box:Achilles-Seeker/Damask-Compute'
